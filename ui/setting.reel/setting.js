a17db6ee424eecbbf8042f4a28540d8f4d2d8037
ji2unjang <noreply>
2017-03-30 06:24:20 +0000

    Add component setting

---
+++ ui/setting.reel/setting.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/setting.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Setting
+ * @extends Component
+ */
+exports.Setting = Component.specialize(/** @lends Setting# */ {
+    constructor: {
+        value: function Setting() {
+            this.super();
+        }
+    }
+});
